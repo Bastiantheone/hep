342931ccc3e8671e1f3b03b996d5679adc8ba4b2
Bastian Wieck <b.wieck@gmx.de>
2017-08-22 10:23:15 -0400

    fastjet: handle rapidity = Inf/-Inf

--- fastjet/jet.go
+++ fastjet/jet.go
@@ -10,11 +10,11 @@ import (
 	"go-hep.org/x/hep/fmom"
 )
 
-// // Used to protect against parton-level events where pt can be zero
-// // for some partons, giving rapidity=infinity. KtJet fails in those cases.
-// const (
-// 	MaxRap = 1e5
-// )
+// Used to protect against parton-level events where pt can be zero
+// for some partons, giving rapidity=infinity. KtJet fails in those cases.
+const (
+	MaxRap = 1e5
+)
 
 // UserInfo holds extra user information in a Jet
 type UserInfo interface{}
@@ -47,7 +47,19 @@ func (jet *Jet) setupCache() {
 	pt := jet.Pt()
 	jet.pt2 = pt * pt
 
-	jet.rap = 0.5 * math.Log((jet.E()+jet.Pz())/(jet.E()-jet.Pz()))
+	if jet.E() == math.Abs(jet.Pz()) {
+		// Point has infinite rapidity -- convert that into a very large
+		// number, but in such a way that different 0-pt momenta will have
+		// different rapidities (so as to lift the degeneracy between them)
+		// [this can be relevant at parton-level]
+		maxRapHere := MaxRap + math.Abs(jet.Pz())
+		if math.Signbit(jet.Pz()) {
+			maxRapHere = -maxRapHere
+		}
+		jet.rap = maxRapHere
+	} else {
+		jet.rap = 0.5 * math.Log((jet.E()+jet.Pz())/(jet.E()-jet.Pz()))
+	}
 	jet.phi = jet.PxPyPzE.Phi()
 }
 
